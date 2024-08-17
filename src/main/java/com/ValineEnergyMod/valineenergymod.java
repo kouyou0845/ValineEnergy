09e2f987b1701541db2d326670275083bcc3766e
kouyou0845 <caedesav@gmail.com>
2024-08-17 21:51:57 +0900

    ValineEnergyMod

--- src/main/java/com/ValineEnergyMod/valineenergymod.java
+++ src/main/java/com/ValineEnergyMod/valineenergymod.java
@@ -18,13 +18,13 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("ValineEnergyMod")
-public class ValineEnergyMod
+@Mod("valineenergymod")
+public class valineenergymod
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ValineEnergyMod()
+    public valineenergymod()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
